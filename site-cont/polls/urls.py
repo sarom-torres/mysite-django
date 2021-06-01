5fc120b6327ac4ed7c2f749ef012debcac7024f1
sarom-torres <saromtorres@gmail.com>
2021-06-01 11:12:43 -0300

    Usando views genericas

--- site-cont/polls/urls.py
+++ site-cont/polls/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'polls' # configura o namespace da aplicação
 urlpatterns = [
     # ex:/polls/
-    path('',views.index, name='index'),
+    path('',views.IndexView.as_view(), name='index'),
     # ex:/polls/34/
     #name pode ser usado para refereciar o caminho nos templates
-    path('<int:question_id>/',views.detail,name='detail'),
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
     # ex:/polls/34/results
-    path('<int:question_id>/results',views.results,name='results'),
+    path('<int:pk>/results',views.ResultsView.as_view(),name='results'),
     # ex:/polls/34/vote
     path('<int:question_id>/vote',views.vote,name='vote')
     ]
--- site-cont/polls/views.py
+++ site-cont/polls/views.py
@@ -1,39 +1,27 @@
-from django import template
-#from django.http.response import Http404
+#from django import template
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import context, loader
 from django.urls import reverse
 from .models import Question, Choice
+from django.views import generic
+#from django.http.response import Http404
+# #from django.template import context, loader
 
+class IndexView(generic.ListView): # ListView abstrai o conceito de exibir uma lista de objetos
+    template_name = 'polls/index.html' #usar um nome de template em vez de auto gerar um.
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    # com atalho
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
-    return render(request,'polls/index.html',context)
+    def get_queryset(self):
+        #retorna as ultima 5 questões publicadas
+        return Question.objects.order_by('-pub_date')[:5]
 
-    #carrega o template e passa um contexto para ele sem atalho
-   
-    #template = loader.get_template('polls/index.html')
-    #context = {'latest_question_list': latest_question_list,}
-    #output = ','.join([q.question_text for q in latest_question_list])
-    #return HttpResponse(template.render(context,request))
+class DetailView(generic.DetailView): # DetailView abstrai o conceito de exibir uma página de detalhe para um tipo particular de objeto
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request, question_id):
-
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-
-    # com atalho
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html' 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
