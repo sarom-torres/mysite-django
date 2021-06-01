60c7e960d603e6bcf8047994fedaf90f2d15e07d
sarom-torres <saromtorres@gmail.com>
2021-06-01 10:31:01 -0300

    Adicionando pagina de resultados

--- site-cont/polls/urls.py
+++ site-cont/polls/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls' # configura o namespace da aplicação
 urlpatterns = [
     # ex:/polls/
     path('',views.index, name='index'),
-    # ex:/polls/5/
+    # ex:/polls/34/
+    #name pode ser usado para refereciar o caminho nos templates
     path('<int:question_id>/',views.detail,name='detail'),
-    # ex:/polls/5/results
+    # ex:/polls/34/results
     path('<int:question_id>/results',views.results,name='results'),
-    # ex:/polls/5/vote
+    # ex:/polls/34/vote
     path('<int:question_id>/vote',views.vote,name='vote')
     ]
--- site-cont/polls/views.py
+++ site-cont/polls/views.py
@@ -1,15 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django import template
+#from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import context, loader
+from django.urls import reverse
+from .models import Question, Choice
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index :-)")
+    # com atalho
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list,}
+    return render(request,'polls/index.html',context)
+
+    #carrega o template e passa um contexto para ele sem atalho
+   
+    #template = loader.get_template('polls/index.html')
+    #context = {'latest_question_list': latest_question_list,}
+    #output = ','.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(template.render(context,request))
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at the question %s." % question_id)
+
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    # com atalho
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of questions %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
